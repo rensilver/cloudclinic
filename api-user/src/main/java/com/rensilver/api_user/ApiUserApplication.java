b9394ef5ba4044edf24378d35790db3fbc8ab381
Renato Silveira <renato.olivbs@gmail.com>
2025-04-22 22:54:11 -0300

    api-user created

---
+++ api-user/src/main/java/com/rensilver/api_user/ApiUserApplication.java
@@ -0,0 +1,13 @@
+package com.rensilver.api_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiUserApplication.class, args);
+	}
+
+}
